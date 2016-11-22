4028e89d65f614f6acae277912280cb643f71536
seven332 <seven332@163.com>
2016-11-23 00:09:10 +0800

    Improve example

--- app/src/main/java/com/hippo/unifile/example/MainActivity.java
+++ app/src/main/java/com/hippo/unifile/example/MainActivity.java
@@ -3,42 +3,60 @@ package com.hippo.unifile.example;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.hippo.unifile.UniFile;
 
-    public static final int REQUEST_CODE_DOCUMENT = 0;
-    public static final int REQUEST_CODE_SELECT = 1;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.action_pick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                Intent intent = new Intent(Intent.ACTION_PICK, uri);
+                startActivityForResult(intent, 0);
+            }
+        });
 
         findViewById(R.id.action_get_content).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("*/*");
-                startActivityForResult(intent, REQUEST_CODE_SELECT);
+                startActivityForResult(intent, 0);
             }
         });
 
+        findViewById(R.id.action_open_document).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.setType("*/*");
+                    startActivityForResult(intent, 0);
+                } else {
+                    Toast.makeText(MainActivity.this, "Only supported in Android4.4+", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         findViewById(R.id.action_open_document_tree).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-                    try {
-                        startActivityForResult(intent, REQUEST_CODE_DOCUMENT);
-                    } catch (ActivityNotFoundException e) {
-                        Toast.makeText(MainActivity.this, "error_cant_find_activity", Toast.LENGTH_SHORT).show();
-                    }
+                    startActivityForResult(intent, 0);
+                } else {
+                    Toast.makeText(MainActivity.this, "Only supported in Android5.0+", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -46,7 +64,7 @@ public class MainActivity extends Activity {
         findViewById(R.id.asset).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO
+                startFileActivity(UniFile.fromAsset(getAssets(), "file2/haha").getUri());
             }
         });
     }
@@ -56,9 +74,21 @@ public class MainActivity extends Activity {
         if (resultCode != Activity.RESULT_OK) {
             return;
         }
+        startFileActivity(data.getData());
+    }
 
+    @Override
+    public void startActivityForResult(Intent intent, int requestCode) {
+        try {
+            super.startActivityForResult(intent, 0);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(MainActivity.this, "error_cant_find_activity", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void startFileActivity(Uri uri) {
         Intent intent = new Intent(this, FileActivity.class);
-        intent.setData(data.getData());
+        intent.setData(uri);
         startActivity(intent);
     }
 }
